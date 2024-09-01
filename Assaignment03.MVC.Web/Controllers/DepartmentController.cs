ef20e81257e2a9c30930c903a011bfc1dc8d0d76
NoranMedhat <NoranMedht63@gmail.com>
2024-09-01 03:12:15 +0300

    Add project files.

---
+++ Assaignment03.MVC.Web/Controllers/DepartmentController.cs
@@ -0,0 +1,21 @@
+﻿using Assignment.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment03.MVC.Web.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentRepository _departmentRepository;
+
+        public DepartmentController(IDepartmentRepository departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var departments= _departmentRepository.GetAll();
+            return View(departments);
+        }
+    }
+}
